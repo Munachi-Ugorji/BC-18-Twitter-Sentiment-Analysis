6f34557f6ed47b9965554a75a411e01956832eab
Munachi-Ugorji <ugorjimunachimso@gmail.com>
2017-01-19 16:12:31 +0100

    Added a text file

---
+++ tests.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var jasmine = require('jasmine').expect;
+
+var words = require('./words.js').words;
+
+describe("wordFrequency()", function() {
+  it("sentences.forEach", function() {
+    var expectedSentence = { [tweets]};
+    expect(wordFrequency("sentences")).toEqual(instanceof Array);
+  });
+
+  
+});
+
